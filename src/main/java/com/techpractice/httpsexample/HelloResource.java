b7843f653668b80ad4eb1bb3704c3afbee0cc5fd
gracia10 <gracia10@daum.net>
2020-06-20 03:51:56 +0900

    Initial commit

---
+++ src/main/java/com/techpractice/httpsexample/HelloResource.java
@@ -0,0 +1,14 @@
+package com.techpractice.httpsexample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloResource {
+    @GetMapping
+    public String hello(){
+        return "hello world";
+    }
+}
---
+++ src/test/java/com/techpractice/httpsexample/HttpsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techpractice.httpsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
